f1f2f82407b62d3b636b7570abd15a6beab97d46
Sonyatchna <sofiyatatchyn@gmail.com>
2020-05-29 01:39:03 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'calculator';
+  private num1: number;
+  private num2: number;
+  private result: number;
+  private resHeading: string;
+  @ViewChild('resultBlock', {static: false}) resultBlock: ElementRef;
+
+  constructor() { }
+
+  private sum(): void {
+    [this.result, this.resHeading] = [this.num1 + this.num2, this.num1 + ' + ' + this.num2];
+    this.resultBlock.nativeElement.style.visibility = 'visible';
+  }
+
+  private diff(): void {
+    [this.result, this.resHeading] = [this.num1 - this.num2, this.num1 + ' - ' + this.num2];
+    this.resultBlock.nativeElement.style.visibility = 'visible';
+  }
+
+  private mult(): void {
+    [this.result, this.resHeading] = [this.num1 * this.num2, this.num1 + ' * ' + this.num2];
+    this.resultBlock.nativeElement.style.visibility = 'visible';
+  }
+
+  private div(): void {
+    [this.result, this.resHeading] = [this.num1 / this.num2, this.num1 + ' / ' + this.num2];
+    this.resultBlock.nativeElement.style.visibility = 'visible';
+  }
 }
